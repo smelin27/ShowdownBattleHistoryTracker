0f55d12650de1b9a2dbf0abff1733f6779640410
smelin27 <epicfire000@gmail.com>
2025-11-04 23:09:11 +1000

    Add-on storage test
    note to self: .then() takes a lambda function, with the stored objects as input. this is important i think. i'm still working this out

--- src/showdown.js
+++ src/showdown.js
@@ -1,1 +1,14 @@
 console.log("Hello world!");
+
+// generic error handler
+function onError(error) { console.log(error); }
+
+browser.storage.local.set({ "a": 1, "b": 2, "c": 3 });
+
+let storageItem = browser.storage.local.get()
+storageItem.then((results) => {
+    console.log(storageItem);
+    console.log(results);
+    console.log(Object.keys(results));
+    console.log(Object.keys(results)[0]);
+}, onError);
